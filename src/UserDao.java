eb4b558e8207c16e50183d1184e96dc0e2336bf7
b.kihoon <masinogns@gmail.com>
2017-04-16 21:49:06 +0900

    statement class 제거를 위한 template callback pattern

--- src/UserDao.java
+++ src/UserDao.java
@@ -13,20 +13,45 @@ public class UserDao {
     JdbcContext jdbcContext;
 
 
-    public User get(String id) throws ClassNotFoundException, SQLException {
-        StatementStrategy statementStrategy = new GetUserStatementStrategy(id);
-        return jdbcContext.jdbcContextWithStatementStratdgyForGet(statementStrategy);
+    public User get(final String id) throws ClassNotFoundException, SQLException {
+        return jdbcContext.jdbcContextWithStatementStratdgyForGet(new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement;
+                preparedStatement = connection.prepareStatement("select * from userinfo where id = ?");
+
+                preparedStatement.setString(1, id);
+
+                return preparedStatement;
+            }
+        });
     }
 
-    public void add(User user) throws ClassNotFoundException, SQLException {
-        StatementStrategy statementStrategy = new AddUserStatementStrategy(user);
+    public void add(final User user) throws ClassNotFoundException, SQLException {
+        jdbcContext.jdbcContextWithStatementStrategyForUpdate(new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement;
+                preparedStatement = connection.prepareStatement("insert into userinfo (id, name, password) VALUE (?,?,?)");
+                preparedStatement.setString(1, user.getId());
+                preparedStatement.setString(2, user.getName());
+                preparedStatement.setString(3, user.getPassword());
 
-        jdbcContext.jdbcContextWithStatementStrategyForUpdate(statementStrategy);
+                return preparedStatement;
+            }
+        });
     }
 
-    public void delete(String id) {
-        StatementStrategy statementStrategy = new DeleteUserStatementStrategy(id);
-        jdbcContext.jdbcContextWithStatementStrategyForUpdate(statementStrategy);
+    public void delete(final String id) {
+        jdbcContext.jdbcContextWithStatementStrategyForUpdate(new StatementStrategy() {
+            @Override
+            public PreparedStatement makeStatement(Connection connection) throws SQLException {
+                PreparedStatement preparedStatement;
+                preparedStatement = connection.prepareStatement("delete from userinfo where id = ?");
+                preparedStatement.setString(1, id);
+                return preparedStatement;
+            }
+        });
     }
 
 }
