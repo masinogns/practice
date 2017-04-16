ff7663d2acf1d4f9d70a55aba4e3b7b24c71f3f4
b.kihoon <masinogns@gmail.com>
2017-04-16 17:29:09 +0900

    user get() and void add() 생성

---
+++ test/UserDaoTest.java
@@ -0,0 +1,46 @@
+import org.junit.Test;
+
+import java.sql.SQLException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+/**
+ * Created by masinogns on 2017. 4. 16..
+ */
+public class UserDaoTest {
+
+    @Test
+    public void get() throws SQLException, ClassNotFoundException {
+
+        String id = "1";
+        String name = "masinogns";
+        String password = "masinogns";
+
+        UserDao userDao = new UserDao();
+        User user = userDao.get(id);
+
+        assertThat(id, is(user.getId()));
+        assertThat(name, is(user.getName()));
+        assertThat(password, is(user.getPassword()));
+    }
+
+    @Test
+    public void add() throws SQLException, ClassNotFoundException {
+        User user = new User();
+        String id = "10";
+        user.setId(id);
+        String name = "rlgnsqor";
+        user.setName(name);
+        String password = "rlgnsqor";
+        user.setPassword(password);
+
+        UserDao userDao = new UserDao();
+        userDao.add(user);
+
+        User addedUser = userDao.get(id);
+        assertThat(id, is(addedUser.getId()));
+        assertThat(name, is(addedUser.getName()));
+        assertThat(password, is(addedUser.getPassword()));
+    }
+}
